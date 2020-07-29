2ec4283ae409deb29c0817342f0c1a2dbe78952d
Christie Molloy <cmolloy@redhat.com>
2020-07-29 15:49:30 -0400

    initiate project

---
+++ src/components/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Services = () => (
+  <div>
+    <h2>Services</h2>
+    <p>
+      This is the services page for the Red Hat Fuse app.
+    </p>
+  </div>
+);
+
+export default Services;
---
+++ src/components/Tools.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Tools = () => (
+  <div>
+    <h2>Tools page</h2>
+    <p>This is the tools page for the Red Hat Fuse app.</p>
+  </div>
+);
+
+export default Tools;
